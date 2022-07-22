64d11245c2449727e6fe980bd172873e9f263734
cwcarithers <caritherschristopher@gmail.com>
2022-07-22 16:25:50 -0500

    fix typer issue

--- ssg.py
+++ ssg.py
@@ -3,10 +3,10 @@ from ssg.site import Site
 
 
 def main(source='content', dest='dist'):
-    config = {"source": source,
-              "dest": dest}
+    config = {"source": source, "dest": dest}
 
     site = Site(**config)
     site.build()
 
-    typer.run(main)
+
+typer.run(main)
